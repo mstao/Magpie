235c8e8ca51e1d09a928be1a5c80f1dff1d43d47
lyfZhixing <2214974097@qq.com>
2018-11-16 18:08:31 +0800

    feat: record browser info

---
+++ magpie-source/src/main/java/me/mingshan/logger/async/source/collector/c1/message/BrowserMessage.java
@@ -0,0 +1,76 @@
+package me.mingshan.logger.async.source.collector.c1.message;
+
+import java.time.LocalDateTime;
+
+/**
+ * record browser info
+ * @author lyf
+ * @date 2018-11-16 17:50:24
+ */
+public class BrowserMessage extends Message {
+
+    /**
+     * ip
+     */
+    private String ipAddress;
+
+    /**
+     * browser / version
+     */
+    private String browser;
+
+    /**
+     * os
+     */
+    private String os;
+
+    /**
+     * http status code
+     */
+    private String status;
+
+    /**
+     * access time
+     */
+    private LocalDateTime createTime;
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
